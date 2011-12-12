a779f3c73748994d4b108b9d6bad1c4d7e075c7e
Sean Wesenberg <wookets@gmail.com>
2011-12-12 13:58:55 -0600

    init commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+var VM = {
+  someVar: "Hello, a binding from main page.",
+  templateParams: {},
+  actualTemplate: KO("main/index"),
+  updateTemplate: false,
+  updateAddress: false
+};
+
+VM.templateMode = function() {
+    var tmpl = VM.actualTemplate();
+    var url = tmpl.split("?");
+    if(url[1] !== null && url[1] !== undefined) {
+      var params = url[1].split("&");
+      for(var i = 0; i < params.length;i++) {
+        param = params[i].split("=");
+        VM.templateParams[param[0]] = param[1];
+      }
+    } else {
+      VM.templateParams = {};
+    }
+    if(VM.updateTemplate !== false) {
+      VM.updateAddress = false;
+      $.address.value(tmpl.replace("/",":"));
+    } else {
+      VM.updateTemplate = true;
+    } 
+    return url[0];
+};
+
+$(function(){
+  ko.externaljQueryTemplateEngine.setOptions({
+        templateUrl: "views",
+        templatePrefix: "",
+        templateSuffix: ".html"
+  });
+  ko.applyBindings(VM);
+  $.address.crawlable(true);
+  $.address.strict(false);
+  $.address.init(function(e) {
+    VM.updateTemplate = true;
+    VM.actualTemplate("main");
+  }).externalChange(function(e){
+      if(VM.updateAddress !== false) {
+        VM.updateTemplate = false;
+        VM.actualTemplate(e.value.replace(/^\//,"").replace(":","/"));
+      } else {
+        VM.updateAddress = true;
+      } 
+  });
+});
